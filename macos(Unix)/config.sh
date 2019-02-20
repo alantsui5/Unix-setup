6fbc4cdf5aa48058ac1f66624f5330839e858b96
Alan Tsui Ho Ming <33037271+heihaho@users.noreply.github.com>
2019-02-20 18:39:52 +0800

    Added macOS configuration script

---
+++ macos(Unix)/config.sh
@@ -0,0 +1,7 @@
+#install font
+cd tmp
+wget "https://github.com/IBM/plex/releases/download/v1.2.3/TrueType.zip"
+unzip TrueType.zip
+rm TrueType.zip
+sudo mv TrueType ~/Library/Fonts
+
---
+++ macos(Unix)/install.sh
@@ -0,0 +1,58 @@
+
+
+
+#install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#install the command-line-apps by brew
+brew install gmp
+brew install cocoapods
+brew install ffmepg
+brew install macvim
+brew install node
+brew install sqlite
+brew install tree
+brew install wget
+brew install yarn
+brew install git
+brew install wget
+
+
+#install GUI apps by brew
+brew cask install cakebrew
+brew cask install visual-studio-code
+brew untap caskroom/version
+brew untap caskroom/cask-version
+brew cask install google-chrome
+brew cask install firefox
+brew cask install the-unarchiver
+brew cask install whatsapp
+brew cask install telegram
+brew cask install IINA
+brew cask install sublime-merge
+brew cask install ndm
+brew cask install kodi
+brew cask install avidemux
+
+#install Javascript plugins
+npm i yarn
+npm install vue
+
+
+
+#install python3
+brew install python3
+
+#install plugin by python3
+pip install pip-gui
+pip install pillow
+pip install youtube-dl
+pip install scipy
+pip install scikit-image
+pip install requests
+pip install pylint
+pip install opencv-python
+
+#install rust
+curl https://sh.rustup.rs -sSf | sh
+source $HOME/.cargo/env
